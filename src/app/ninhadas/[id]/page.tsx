2057700b808cdb0f2de8ad97ded31cd7e11c8cd9
José Augusto <jaugustolt1998@gmail.com>
2026-03-16 20:49:24 -0300

    Force dynamic rendering on public detail pages to fix Vercel build errors

--- src/app/ninhadas/[id]/page.tsx
+++ src/app/ninhadas/[id]/page.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 import Image from "next/image";
 import Link from "next/link";
 
